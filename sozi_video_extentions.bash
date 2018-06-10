174f2fd3669a8fae20828d30812394f21f6377d9
yuzhangbit <yu.zhang.bit@gmail.com>
2018-06-10 17:33:19 +0800

    add sozi media extension install script

---
+++ sozi_video_extentions.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e  # exit on first error
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="/tmp"
+VERSION="18.04-1523042682"
+EXT_NAME="Sozi-extras-media-${VERSION}"
+URL="https://github.com/senshu/Sozi/releases/download/v18.04/${EXT_NAME}.zip"
+
+
+
+main()
+{
+  echo "URL:${URL}"
+  install_sozi_media_extension
+}
+
+
+
+install_sozi_media_extension()
+{
+  ## download zip to temp directory
+  cd ${DIR}
+  ## clean up before installation
+  rm -rf ${EXT_NAME}*
+  ## download
+  wget ${URL}
+  ## extract the content and install them to
+  unzip ${EXT_NAME}.zip -d ~/.config/inkscape/extensions
+  rm -rf ${EXT_NAME}*
+}
+
+
+main
