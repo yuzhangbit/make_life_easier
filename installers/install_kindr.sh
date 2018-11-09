949aa3365c2ceac389fdfcb0db71e34af20a814f
yuzhangbit <yu.zhang.bit@gmail.com>
2018-11-09 23:07:47 +0800

    add deps

--- installers/install_kindr.sh
+++ installers/install_kindr.sh
@@ -2,7 +2,8 @@
 set -e  # exit on first error
 
 main(){
-  sudo apt-get update && sudo apt-get -y install cmake build-essential gcc g++
+  sudo apt-get update && \
+  sudo apt-get -y install cmake build-essential gcc g++ libeigen3-dev
   cd /tmp && rm -rf kindr && git clone --depth 1 --branch 1.1.2 https://github.com/ANYbotics/kindr.git
   cd kindr
   mkdir -p build && cd build && cmake ..
