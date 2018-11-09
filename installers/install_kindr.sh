737379bc4e79ff6879a2a90ad6784c3c35be5f16
yuzhangbit <yu.zhang.bit@gmail.com>
2018-11-09 22:51:50 +0800

    update essential tools

--- installers/install_kindr.sh
+++ installers/install_kindr.sh
@@ -2,6 +2,7 @@
 set -e  # exit on first error
 
 main(){
+  sudo apt-get update && sudo apt-get -y install cmake build-essential gcc g++
   cd /tmp && rm -rf kindr && git clone --depth 1 --branch 1.1.2 https://github.com/ANYbotics/kindr.git
   cd kindr
   mkdir -p build && cd build && cmake ..
