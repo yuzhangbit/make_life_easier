caeb7e4f14a83ecb0ecaa5dda6a7ef576154bf65
yuzhangbit <yu.zhang.bit@gmail.com>
2018-11-09 10:23:53 +0800

    update

--- installers/v2ray_supervisor_setup.sh
+++ installers/v2ray_supervisor_setup.sh
@@ -31,8 +31,7 @@ create_config_for_app()
     # change the permission of the file
     sudo chmod a+x $CONF
     # enable the app in supervisor
-    sudo mkdir -p $CONF_DIR && sudo mv $CONF $CONF_DIR
-    sudo service supervisord start
+    sudo mv $CONF $CONF_DIR
     # read the supervisor configuration file
     sudo supervisorctl reread
     # update the programs run by the supervisor
