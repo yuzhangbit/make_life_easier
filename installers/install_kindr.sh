9d36d694aff34245ed54c9bd40a6f81ac2b13496
yuzhangbit <yu.zhang.bit@gmail.com>
2018-11-09 22:30:56 +0800

    use https

--- installers/install_kindr.sh
+++ installers/install_kindr.sh
@@ -2,7 +2,7 @@
 set -e  # exit on first error
 
 main(){
-  cd /tmp && rm -rf kindr && git clone --depth 1 --branch 1.1.2 git@github.com:anybotics/kindr.git
+  cd /tmp && rm -rf kindr && git clone --depth 1 --branch 1.1.2 https://github.com/ANYbotics/kindr.git
   cd kindr
   mkdir -p build && cd build && cmake ..
   make -j4
